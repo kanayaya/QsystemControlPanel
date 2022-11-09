d41bb8827373f60bec41e550391aea348877fb65
kanayaya <kanayaya@mail.ru>
2022-11-09 18:55:31 +0800

    add QAPI lib

---
+++ src/main/java/com/kanayaya/QsystemControlPanel/test.java
@@ -0,0 +1,11 @@
+package com.kanayaya.QsystemControlPanel;
+
+import ru.apertum.qsystem.common.QConfig;
+
+public class test {
+    public static void main(String[] args) {
+        QConfig config = new QConfig() {
+        }
+        QConfig.setCfg();
+    }
+}
